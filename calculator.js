f2348da6bbda1f568f98345d036422ce370ff342
MeDeveloperWeb <www.shivachaurasia@gmail.com>
2023-10-11 23:27:31 +0530

    Calculator logic works

--- calculator.js
+++ calculator.js
@@ -6,6 +6,13 @@ const operators = document.querySelectorAll(".operators-container > span");
 let ansHistory = [];
 let operator, operand;
 
+// know if the operand has already been calculated
+let operandIsResult = false;
+
+/**
+ * Play button sound
+ * link buttons to specified functions
+ */
 document.querySelector(".keys-container").addEventListener('click', (e) => {
     if (e.target.tagName === "BUTTON") {
         audio.play();
@@ -13,6 +20,11 @@ document.querySelector(".keys-container").addEventListener('click', (e) => {
     }
 });
 
+/**
+ * 
+ * @param {number} num 
+ * @returns {number} digits in given number
+ */
 function getDigitCount(num) {
     let digCount = 0;
     let numString = num.toString();
@@ -22,86 +34,14 @@ function getDigitCount(num) {
     return digCount;
 }
 
-function isUnderflow(num) {
-    if (num < 0.000000000000001) return true;
-    return false;
-}
-
-function isOverflow(num) {
-    if (num > 9999999999999999n) return true;
-    return false;
-}
-
-function handleButtons(btn) {
-    if (+btn.id === 0 || +btn.id) {
-        if (operator) {
-            operand = evaluate(+operand, +screen.textContent, operator);
-            operator = "";
-            screen.textContent = "";
-        }
-        screen.textContent += btn.id;
-    }
-    else {
-        switch (btn.id) {
-            case "AC":
-                ansHistory = [];
-                screen.textContent = "";
-                break;
-            
-            case "GT":
-                let grandTotal = 0;
-                ansHistory.forEach(num => grandTotal += num);
-                screen.textContent = grandTotal;
-                break;
-
-            case "clear":
-                screen.textContent = "";
-                break;
-            
-            case "delete":
-                screen.textContent = screen.textContent.slice(0, -1);
-
-            case "plus-minus":
-                if (screen.textContent[0] === '-') screen.textContent = screen.textContent.slice(1);
-                else screen.textContent = '-' + screen.textContent;
-                break;
-
-            case "dec":
-                if (!screen.textContent.includes('.')) screen.textContent += '.';
-                break;
-
-            case "equals":
-                try {
-                    screen.textContent = evaluate(+operand, +screen.textContent, operator);
-                }
-                catch (err){
-                    screen.textContent = err;
-                    return;
-                }
-                ansHistory.push(screen.textContent);
-                operand = operator = "";
-                break;
-            
-            case "sqrt":
-                let sqrt = Math.sqrt(+screen.textContent);
-                if (isUnderflow(sqrt)) {
-                    screen.textContent = "underflow";
-                    break;
-                }
-                if (getDigitCount(sqrt) > 16) sqrt = sqrt.toPrecision(16);
-                screen.textContent = sqrt;
-                break;
-
-            default:
-                operator = btn.id;
-                operators.forEach(op => {
-                    if (op.className === btn.id) op.style.visibility = "visible";
-                    else op.style.visibility = "hidden";
-                });
-        }
-    }
-}
-
+/**
+ * 
+ * @param {number} operand1 
+ * @param {number} operand2 
+ * @param {string} operator 
+ * @returns evaluates given expression
+ * @throws Error("Math.exe crashed") on division by 0
+ */
 function evaluate(operand1, operand2, operator) {
     switch (operator) {
         case "plus":
@@ -115,12 +55,158 @@ function evaluate(operand1, operand2, operator) {
         
         case "div":
             if (operand2 === 0) {
-                throw Error("Math.exe stopped working");
+                throw Error("Math.exe crashed");
             }
             return operand1 / operand2;
         
         case "perc":
             return operand1 * operand2 / 100;
 
+        case "sqrt":
+            return Math.sqrt(operand1);
+
+        default:
+            return operand2;
+
+    }
+}
+
+/**
+ * 
+ * @param {number} operand1 
+ * @param {number} operand2 
+ * @param {string} operator 
+ * @returns evaluated expression having digits less than 16
+ * @throws caught error from 'evaluate'
+ */
+function getFormattedEvaluation(operand1, operand2, operator) {
+    try {
+        let result = evaluate(operand1, operand2, operator).toPrecision(15);
+
+        // For exponent result setting digits after decimal of normalizer to 8
+        if (result.includes('e')) return (+result).toExponential(8);
+        // If the no. of digit are still greater than 15 then,
+        // number is in the form '0.something'
+        // Fixing the number of digit after decimal point to make result have digits < 16
+        else if (getDigitCount(result) > 15) return (+result).toFixed(14);
+
+        return +result;
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+/**
+ * Implements basic calculator logic
+ * @param {HTMLButtonElement} btn 
+ */
+function handleButtons(btn) {
+    if (+btn.id === 0 || +btn.id || btn.id === 'dec') {
+        if (operandIsResult || isNaN(screen.textContent)) {
+            // Reset the input if the result is displayed
+            if (operator === "equals") {
+                operator = "";
+                // Remove equal sign from the screen
+                operators.forEach(op => op.style.visibility = "hidden");
+            }
+            // - is NaN but we don't want to remove it
+            if (screen.textContent !== "-") screen.textContent = "";
+        }
+
+        operandIsResult = false;
+
+        // Input number
+        if (getDigitCount(+screen.textContent) < 15) {
+
+            if (btn.id === 'dec') {
+                if (screen.textContent === "") screen.textContent = "0."
+                else if (!screen.textContent.includes('.')) screen.textContent += '.';
+            }
+            else screen.textContent += btn.id;
+        }
+    }
+    else {
+        switch (btn.id) {
+            case "AC":
+                ansHistory = [];
+                screen.textContent = operand = operator = "";
+                operandIsResult = false;
+                operators.forEach(op => op.style.visibility = "hidden");
+                break;
+
+            case "clear":
+                screen.textContent = "";
+                break;
+            
+            case "delete":
+                // Delete number 1 by 1
+                if (!operandIsResult || !isNaN(screen.textContent)) {
+                    screen.textContent = screen.textContent.slice(0, -1);
+                    break;
+                }
+                // if error or result is shown on screen
+                screen.textContent = "";
+                break;
+                
+
+            case "plus-minus":
+                // Remove content from screen
+                if (operandIsResult || isNaN(screen.textContent)) screen.textContent = "";
+                // Add or remove - to/from screen
+                if (screen.textContent[0] === '-') screen.textContent = screen.textContent.slice(1);
+                else screen.textContent = '-' + screen.textContent;
+                break;
+            
+            case "sqrt":
+                if (operandIsResult) break; 
+                let sqrt = getFormattedEvaluation(+screen.textContent, '', 'sqrt');
+                screen.textContent = sqrt;
+                break;
+
+            case "GT":
+                let grandTotal = 0;
+                ansHistory.forEach(num => grandTotal += num);
+                screen.textContent = grandTotal;
+                operator = "equals";
+
+                operators.forEach(op => {
+                    if (op.className === "equals") op.style.visibility = "visible";
+                    else op.style.visibility = "hidden";
+                });
+                break;
+
+            // An operator is clicked
+            default:
+                try {
+                    // Evaluate previous operation
+                    if(!operandIsResult || operator === "equals")
+                        screen.textContent = operand = getFormattedEvaluation(+operand, +screen.textContent, operator);
+
+                    operandIsResult = true;
+                    // Store new operator
+                    operator = btn.id;
+
+                    if (operator === "equals") {
+                        ansHistory.push(operand);
+                        operand = "";
+                    }
+                }
+                catch (err) {
+                    // Math Error
+                    // Breaking functioning in between
+                    // Same functioning when equal is clicked
+                    operand = "";
+                    operator = "equals";
+                    operandIsResult = false;
+                    screen.textContent = err.toString().slice(6);
+                }
+
+                // Show the clicked operator, Hide all other
+                operators.forEach(op => {
+                    if (op.className === btn.id) op.style.visibility = "visible";
+                    else op.style.visibility = "hidden";
+                });
+        }
     }
 }
